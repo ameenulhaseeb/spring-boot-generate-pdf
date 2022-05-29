f992684d2c956841982ce3347605af38e7ab070a
ameenulhaseeb <ameenulhaseeb@gmail.com>
2022-05-29 14:44:01 +0530

    1st changes

--- src/main/java/dev/simplesolution/pdf/service/impl/PdfGenerateServiceImpl.java
+++ src/main/java/dev/simplesolution/pdf/service/impl/PdfGenerateServiceImpl.java
@@ -2,6 +2,8 @@ package dev.simplesolution.pdf.service.impl;
 
 import com.lowagie.text.DocumentException;
 import dev.simplesolution.pdf.service.PdfGenerateService;
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.xhtmlrenderer.pdf.ITextRenderer;
+import org.xhtmlrenderer.pdf.PDFEncryption;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -32,12 +35,19 @@ public class PdfGenerateServiceImpl implements PdfGenerateService {
 
         String htmlContent = templateEngine.process(templateName, context);
         try {
+        	//final File outputFile = File.createTempFile(fileName, ".pdf");
+            //PDFEncryption pdfEncryption  = new PDFEncryption();
+        	String password= "password@123";
+        	//pdfEncryption.setUserPassword(password.getBytes());
             FileOutputStream fileOutputStream = new FileOutputStream(pdfDirectory + pdfFileName);
             ITextRenderer renderer = new ITextRenderer();
+          
             renderer.setDocumentFromString(htmlContent);
             renderer.layout();
+            //renderer.setPDFEncryption(pdfEncryption);
             renderer.createPDF(fileOutputStream, false);
             renderer.finishPDF();
+
         } catch (FileNotFoundException e) {
             logger.error(e.getMessage(), e);
         } catch (DocumentException e) {
